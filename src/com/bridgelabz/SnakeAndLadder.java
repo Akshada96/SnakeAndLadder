0d7e9dafb81aa564ec25516fdbe0740dc366c24e
Akshada <akshumore96@gmail.com>
2022-03-15 18:03:54 +0530

    Added UC-6-TotalDiceRolls

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -7,31 +7,34 @@ public class SnakeAndLadder {
 
     public static void main(String[] args){
         int position = 0;
+        int rolls = 0;
         while (position < 100) {
             int dice = (int) (Math.random()*6)+1;
-            System.out.println("No on dice : "+dice);
+            //System.out.println("No on dice : "+dice);
+            rolls++;
             int option = (int) (Math.random()*3);
             //System.out.println("Option : "+option);
             switch (option) {
                 case LADDER :
-                    System.out.println("Ladder");
+                    //System.out.println("Ladder");
                     position = position + dice;
                     break;
                 case SNAKE :
-                    System.out.println("Snake");
+                    //System.out.println("Snake");
                     position = position - dice;
                     break;
                 case NO_PLAY :
-                    System.out.println("No Play");
+                    //System.out.println("No Play");
                     break;
             }
-            System.out.println("Position : "+position);
             if (position<0) {
                 position = 0;
             }
             if (position > 100) {
                 position = position - dice;
             }
+            System.out.println("Position : "+position);
         }
+        System.out.println("Total dice rolls : "+rolls);
     }
 }
