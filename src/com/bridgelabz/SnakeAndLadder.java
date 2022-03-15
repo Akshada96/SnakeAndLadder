1ebd779db68a787bcfc3d29c6838e1753934c749
Akshada <akshumore96@gmail.com>
2022-03-15 17:09:17 +0530

    Added UC-2-DiceRoll

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -5,5 +5,7 @@ public class SnakeAndLadder {
         System.out.println("Snake & Ladder Simulator");
         String player1;
         int position = 0;
+        int dice = (int) (Math.random()*6)+1;
+        System.out.println("No of dice : "+dice);
     }
 }
