92d9790a07fb27e680c4e8eac5748e5daf9cce9c
Akshada <akshumore96@gmail.com>
2022-03-15 17:26:49 +0530

    Added UC-3-OptionCheck

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,11 +1,24 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
+    static final int NO_PLAY = 0;
+    static final int LADDER = 1;
+    static final int SNAKE = 2;
+    
     public static void main(String[] args){
-        System.out.println("Snake & Ladder Simulator");
-        String player1;
         int position = 0;
         int dice = (int) (Math.random()*6)+1;
         System.out.println("No of dice : "+dice);
+        int option = (int) (Math.random()*3);
+        System.out.println("Option : "+option);
+        switch (option) {
+            case LADDER :
+                position = position + dice;
+                break;
+            case SNAKE :
+                position = position - dice;
+                break;
+        }
+        System.out.println("Position : "+position);
     }
 }
